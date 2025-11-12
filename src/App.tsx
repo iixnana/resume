89b43f55df276e44aead1cbe48c01140b6d5cbd3
kamile <kamile@krafio.com>
2025-11-12 18:27:59 +0100

    add logo and cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '/resumelogo.svg'
 import './App.css'
+import { motion } from "motion/react"
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <motion.img
+        src={Logo}
+        alt="Kamile's Resume Logo"
+        className="logo"
+        drag
+        whileDrag={{ scale: 1.05, cursor: "grabbing" }}
+        style={{ width: 240, borderRadius: 12, userSelect: "none", touchAction: "none" }}
+        draggable={false} // prevent the browser's native image drag ghost
+        dragConstraints={{
+          top: -100,
+          left: -100,
+          right: 100,
+          bottom: 100,
+        }}
+        dragElastic={0.2}
+      />
+      <h1>Kamile's Resume</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
