f397de649b6dd84d83532112ea3d415f68b9f274
iixnana <kamile.nanartonyte@gmail.com>
2025-11-19 17:33:13 +0100

    added eslint rules, updated readme, added theme toggle button

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -10,9 +11,12 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
