2d1eed130ca96467d53b5c89fd98a8585e3c0956
iixnana <kamile.nanartonyte@gmail.com>
2025-11-22 17:34:38 +0100

    adding light bg

--- src/app/layout/PageLayout.tsx
+++ src/app/layout/PageLayout.tsx
@@ -1,19 +1,26 @@
 import type { PropsWithChildren } from "react";
 import { ThemeToggle } from "../components/ThemeToggle/ThemeToggle";
+import { useTheme } from "../../context/theme/ThemeContext.hook";
 import "./PageLayout.css";
 import "../../style/background-dark.scss";
-import { useTheme } from "../../context/theme/ThemeContext.hook";
+import "../../style/background-light.scss";
 
 export function PageLayout({ children }: PropsWithChildren) {
   const { isDark } = useTheme();
 
   return (
     <main>
-      {isDark && (
+      {isDark ? (
         <div id="star-bg">
           <div className="stars-small" />
           <div className="stars-medium" />
           <div className="stars-big" />
+        </div>
+      ) : (
+        <div id="comet-bg">
+          <div className="comets-small"></div>
+          <div className="comets-medium"></div>
+          <div className="comets-big"></div>
         </div>
       )}
 
