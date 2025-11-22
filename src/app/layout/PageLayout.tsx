6f4d53f35ccd0ac41c04e3b457611e2a6d7ff0db
iixnana <kamile.nanartonyte@gmail.com>
2025-11-22 18:01:27 +0100

    bg the same for both themes

--- src/app/layout/PageLayout.tsx
+++ src/app/layout/PageLayout.tsx
@@ -1,29 +1,16 @@
 import type { PropsWithChildren } from "react";
 import { ThemeToggle } from "../components/ThemeToggle/ThemeToggle";
-import { useTheme } from "../../context/theme/ThemeContext.hook";
 import "./PageLayout.css";
-import "../../style/background-dark.scss";
-import "../../style/background-light.scss";
+import "../../style/background.scss";
 
 export function PageLayout({ children }: PropsWithChildren) {
-  const { isDark } = useTheme();
-
   return (
     <main>
-      {isDark ? (
-        <div id="star-bg">
-          <div className="stars-small" />
-          <div className="stars-medium" />
-          <div className="stars-big" />
-        </div>
-      ) : (
-        <div id="comet-bg">
-          <div className="comets-small"></div>
-          <div className="comets-medium"></div>
-          <div className="comets-big"></div>
-        </div>
-      )}
-
+      <div id="star-bg">
+        <div className="stars-small" />
+        <div className="stars-medium" />
+        <div className="stars-big" />
+      </div>
       <h1 className="screen-reader-only">Resume of Kamile Nanartonyte</h1>
       <ThemeToggle />
       {children}
