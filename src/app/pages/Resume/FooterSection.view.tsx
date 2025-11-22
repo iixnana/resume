b77436db6abe86ea54ba1e0549d0f5caa62cb2d4
iixnana <kamile.nanartonyte@gmail.com>
2025-11-22 23:24:40 +0100

    organizing, added scroll progress bar

---
+++ src/app/pages/Resume/FooterSection.view.tsx
@@ -0,0 +1,8 @@
+export const FooterSection = () => {
+  return (
+    <section id="footer" className="footer">
+      <div>(┛❍ᴥ❍ )┛彡┻━┻</div>
+      <div className="author">Kamile @ 2025</div>
+    </section>
+  );
+};
---
+++ src/app/pages/Resume/HeaderSection.view.tsx
@@ -0,0 +1,56 @@
+import { Computer } from "../../components/Computer/Computer";
+import { ResumeLogo } from "../../components/ResumeLogo/ResumeLogo";
+import { useRef } from "react";
+import type { Project } from "../../../types/project";
+import GithubLogo from "/github-mark.svg";
+
+interface HeaderSectionProps {
+  project: Project;
+}
+
+export const HeaderSection = ({ project }: HeaderSectionProps) => {
+  const constraintsRef = useRef(null);
+
+  return (
+    <>
+      <section id="welcome" className="welcome-section">
+        <div className="welcome-box" ref={constraintsRef}>
+          <div className="welcome-box-inner">
+            <ResumeLogo constraintsRef={constraintsRef} />
+            <h2 className="welcome-h2">Hello, world!</h2>
+            <div>＼（＾▽＾）／</div>
+            <p className="welcome-description">
+              Thank you for visiting my website. It took quite a while to build,
+              therefore each visitor is greaty appreciated! This was built for
+              fun, therefore there are a few fun features.
+              <br />
+              <b>First tip:</b> Try moving the logo.
+              <br />
+              Enjoy! o(^▽^)o
+            </p>
+            <a
+              href="/kamile-nanartonyte-resume.pdf"
+              download
+              className="download-button"
+            >
+              Download Resume
+            </a>
+            <div className="git">
+              <a href={project.repo} target="_blank" className="git-link">
+                Code repository
+              </a>
+              <img src={GithubLogo} className="git-logo" />
+              <a href={project.issues} target="_blank" className="git-link">
+                Report issues
+              </a>
+            </div>
+          </div>
+        </div>
+        <Computer />
+      </section>
+      <div id="table-decoration" className="table">
+        <div className="table-inner" />
+      </div>
+    </>
+  );
+};
