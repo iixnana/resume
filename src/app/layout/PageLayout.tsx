7712e3e303e219a5f09d71df5d25527f66336871
iixnana <kamile.nanartonyte@gmail.com>
2025-11-22 17:00:01 +0100

    fixed dark bg

--- src/app/layout/PageLayout.tsx
+++ src/app/layout/PageLayout.tsx
@@ -1,10 +1,22 @@
 import type { PropsWithChildren } from "react";
 import { ThemeToggle } from "../components/ThemeToggle/ThemeToggle";
 import "./PageLayout.css";
+import "../../style/background-dark.scss";
+import { useTheme } from "../../context/theme/ThemeContext.hook";
 
 export function PageLayout({ children }: PropsWithChildren) {
+  const { isDark } = useTheme();
+
   return (
     <main>
+      {isDark && (
+        <div id="star-bg">
+          <div className="stars-small" />
+          <div className="stars-medium" />
+          <div className="stars-big" />
+        </div>
+      )}
+
       <h1 className="screen-reader-only">Resume of Kamile Nanartonyte</h1>
       <ThemeToggle />
       {children}
